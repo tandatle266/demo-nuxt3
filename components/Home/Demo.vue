a47af2495383fe67733b17f9be3a866986e0bc79
tandatle266 <45076955+tandatle266@users.noreply.github.com>
2024-08-09 16:41:58 +0700

    Update Demo.vue

--- components/Home/Demo.vue
+++ components/Home/Demo.vue
@@ -13,8 +13,17 @@
 <script>
 export default {
 	name: 'HomeDemo',
-	mounted() {
+	async mounted() {
 		console.log('HomeDemo mounted')
+
+		try {
+			const config = await useFetch("/api/checkout", {
+				method: "POST",
+				body: JSON.stringify({})
+			});
+			console.log(config, "config")
+		} catch(err) { console.log(err, "err") }
+		
 	}
 };
 </script>
